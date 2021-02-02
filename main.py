cc8ed54a4e1eccc686fef705e56007eb1a38d97d
Nate Anderson <n8anderson@student.bridgew.edu>
2021-02-02 09:42:26 -0600

    fixed bug with printing actual school

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ def get_date(url:str):
     return all_data
 
 def main():
-    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.name,2013.student.size"
+    url = "https://api.data.gov/ed/collegescorecard/v1/schools.json?school.degrees_awarded.predominant=2,3&fields=id,school.state,school.name"
     all_data = get_date(url)
     for item in all_data:
         print(item)
