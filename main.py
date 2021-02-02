f909972e5e50d3b391e5e52076a13d47bf43cad5
Nate Anderson <n8anderson@student.bridgew.edu>
2021-02-02 09:03:17 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+
+def get_date(url:str):
+    all_data = []
+
+
+    return all_data
+
+def main():
+    url = "https://api.data.gov/ed/collegescorecard"
+    all_data = get_date(url)
+
+#If running to get functions dont run main
+#If running to run, run main
+if __name__ == '__main__':
+    main()
